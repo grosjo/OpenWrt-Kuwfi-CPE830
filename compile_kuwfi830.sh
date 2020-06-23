c474a0b4395a4af27815c5d51218e47d1eb78072
Joan Moreau <jom@grosjo.net>
2020-06-23 21:03:46 +0100

    Update compile_kuwfi830.sh

--- compile_kuwfi830.sh
+++ compile_kuwfi830.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 rm -rf kuwfi830
 git clone https://git.openwrt.org/openwrt/openwrt.git kuwfi830
-rm 2362.patch
-wget https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/2362.patch
+rm 3133.patch
+wget https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/3133.patch
 cp config_kuwfi830 kuwfi830/.config
 cd kuwfi830
-patch -p1 < ../2362.patch
+patch -p1 < ../3133.patch
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 cd ..
