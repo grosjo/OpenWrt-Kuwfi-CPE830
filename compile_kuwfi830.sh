546ad595383bfb46a5b95864b787fb1691fa461c
Joan Moreau <jom@grosjo.net>
2024-09-21 22:05:55 +0800

    Update compile_kuwfi830.sh

--- compile_kuwfi830.sh
+++ compile_kuwfi830.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 rm -rf kuwfi830
 git clone https://git.openwrt.org/openwrt/openwrt.git kuwfi830
-rm 3133.patch
-wget https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/3133.patch
+
+rm openwrt.patch
+wget https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/15969.diff -O openwrt.patch
+
 cp config_kuwfi830 kuwfi830/.config
 cd kuwfi830
-patch -p1 < ../3133.patch
-./scripts/feeds update -a
-./scripts/feeds install -a
+echo "Patching"
+patch -p1 < ../openwrt.patch
+
+./scripts/feeds update -a 
+./scripts/feeds install -a 
 cd ..
 cp config_kuwfi830 kuwfi830/.config
 chown -R admin:admin kuwfi830
 cd kuwfi830
+
 sudo -u admin make defconfig
-sudo -u admin make -j16 download
-sudo -u admin make -j16
+sudo -u admin make -j 16 download
+sudo -u admin make -j 16
 cd ..
-gzip -9 kuwfi830/bin/targets/ath79/generic/openwrt-ath79-generic-yuncore_cpe830-squashfs-sysupgrade.bin
-mv kuwfi830/bin/targets/ath79/generic/openwrt-ath79-generic-yuncore_cpe830-squashfs-sysupgrade.bin.gz WHEREYOUWANT
+mv kuwfi830/bin/targets/ath79/generic/openwrt-ath79-generic-yuncore_cpe830-squashfs-sysupgrade.bin /WHEREYOUWANT/
+mv kuwfi830/bin/targets/ath79/generic/openwrt-ath79-generic-yuncore_cpe830-squashfs-tftp.bin /WHEREYOUWANT/
